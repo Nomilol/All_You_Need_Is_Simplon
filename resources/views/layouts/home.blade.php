72f688607f960f9e66786324c9861f90b53e0922
Axel <lezinaxel@gmail.com>
2017-02-21 16:46:08 +0100

    Création du header et du bloc " Je suis Simplonien "

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.8/semantic.min.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <img src="http://placehold.it/140x100" alt=""><h1>SIMPLON pour le moment</h1>
+      </header>
+      <div class="description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="iAmSimplonien">
+          <a href="#">Ajouter / Editer</a>
+        </div>
+      </div>
+
+
+
+      <footer>
+
+      </footer>
+    </div>
+  </body>
+</html>
