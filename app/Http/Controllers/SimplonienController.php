bbb25172d37ba34176ab57b3132e55a6f3e2f913
Juliane Blier <blier.juliane@gmail.com>
2017-02-22 21:28:41 +0100

    Méthode d'ajout en db du formulaire

--- app/Http/Controllers/SimplonienController.php
+++ app/Http/Controllers/SimplonienController.php
@@ -33,4 +33,27 @@ class SimplonienController extends Controller
     public function sendAddMail($email){
         Mail::to($email)->send(new AddProfile($email));
     }
+
+    public function addSimplonien(Request $request){
+        $simplonien = new Simplonien;
+        $simplonien->nom = $request->nom;
+        $simplonien->prenom = $request->prenom;
+        $simplonien->email = $request->email;
+        $simplonien->telephone = $request->telephone;
+        $simplonien->code_postal = $request->code_postal;
+        $simplonien->ville_formation = $request->ville_formation;
+        $simplonien->promo = $request->promo;
+        $simplonien->github = $request->github;
+        $simplonien->cv = $request->cv;
+        $simplonien->punchline = $request->punchline;
+        $simplonien->linkedin = $request->linkedin;
+        $simplonien->twitter = $request->twitter;
+        $simplonien->facebook = $request->facebook;
+        $simplonien->site_perso = $request->site_perso;
+        $simplonien->blog = $request->blog;
+
+        $simplonien->save();
+
+        return view('simplonien.home');
+    }
 }
