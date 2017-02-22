cfc45f9bf9c681cfd40fa4e444885c92d1c88e48
Juliane Blier <blier.juliane@gmail.com>
2017-02-22 12:05:18 +0100

    Ajout de la méthode d'envoi de mail

--- app/Http/Controllers/SimplonienController.php
+++ app/Http/Controllers/SimplonienController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Simplonien;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\AddProfile;
 
 class SimplonienController extends Controller
 {
@@ -14,7 +16,12 @@ class SimplonienController extends Controller
             return view('simplonien.edit');
         }
         else {
+            $this->sendAddMail($testedEmail);
             return view('simplonien.add');
         }
+    }
+
+    public function sendAddMail($email){
+        Mail::to($email)->send(new AddProfile());
     }
 }
