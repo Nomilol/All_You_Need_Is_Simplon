c3d6e74f36d8a53afb4520daa47e90a006c5f1c1
Juliane Blier <blier.juliane@gmail.com>
2017-02-23 16:49:11 +0100

    Création de la table des coordinates

---
+++ database/migrations/2017_02_23_152952_create_coordinates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoordinatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coordinates', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('code_postal');
+            $table->decimal('longitude', 15, 7);
+            $table->decimal('latitude', 15, 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
