ca624a8fd0442f85af5b06003a2576d1c1bfd094
Juliane Blier <blier.juliane@gmail.com>
2017-02-23 09:11:44 +0100

    Adresse locale Simplon dans le mail

--- resources/views/mails/addProfile.blade.php
+++ resources/views/mails/addProfile.blade.php
@@ -1,5 +1,5 @@
 <h3>Ajouter un profil de Simplonien</h3>
 
-<p>Vous pouvez ajouter le profil en suivant ce lien : http://192.168.1.14:8000/verifyToken?token={{ $nonce }} </p>
+<p>Vous pouvez ajouter le profil en suivant ce lien : http://192.168.1.32:8000/verifyToken?token={{ $nonce }} </p>
 
 <p>L'équipe AYNIS</p>
