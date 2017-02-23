f878dfc067106438be30f2ee1d0c2484c058c580
Juliane Blier <blier.juliane@gmail.com>
2017-02-23 14:16:24 +0100

    Ajout du template du mail d'édition

---
+++ resources/views/mails/edit.blade.php
@@ -0,0 +1,5 @@
+<h3>Editer mon profil de Simplonien</h3>
+
+<p>Vous pouvez éditer votre profil en suivant ce lien : http://192.168.1.32:8000/verifyToken?token={{ $nonce }} </p>
+
+<p>L'équipe AYNIS</p>
